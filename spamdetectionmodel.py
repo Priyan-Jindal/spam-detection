34d59b577a123a920bba79df372ed838855b5c97
Priyan-Jindal <98865195+Priyan-Jindal@users.noreply.github.com>
2023-11-15 14:15:25 -0800

    added comments for readability

--- spamdetectionmodel.py
+++ spamdetectionmodel.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import pytorch_lightning as pl
 import torch as th
 from torch.utils import data as th_data
@@ -6,32 +7,42 @@ from sklearn.model_selection import train_test_split
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from datasets import load_dataset
 
+# Load pre-trained pipeline for text classification
 pipe = pipeline("text-classification", model="mrm8488/bert-tiny-finetuned-sms-spam-detection")
 
-from sklearn.model_selection import train_test_split
-
-
-
+# Define the Spam Detection Model using PyTorch Lightning
 class SpamDetectionModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
+        
+        # Load pre-trained BERT model for sequence classification
         self.model = AutoModelForSequenceClassification.from_pretrained("mrm8488/bert-tiny-finetuned-sms-spam-detection")
+        
+        # Define the loss function
         self.loss = th.nn.CrossEntropyLoss(reduction='none')
-        self.seq_length = 102 # 95th percentile
+        
+        # Define hyperparameters
+        self.seq_length = 102  # 95th percentile
         self.batch_size = 15
         self.learning_rate = 2e-4
         self.epochs = 5
+        
+        # Lists to store loss and accuracy outputs during validation
         self.loss_outputs = []
         self.acc_outputs = []
 
     def prepare_data(self):
+        # Load tokenizer
         tokenizer = AutoTokenizer.from_pretrained("mrm8488/bert-tiny-finetuned-sms-spam-detection")
 
+        # Tokenization function
         def _tokenize(data):
             data['input_ids'] = tokenizer.batch_encode_plus(data['messageBody'], max_length=self.batch_size, pad_to_max_length=True)['input_ids']
             return data
 
+        # Dataset preparation function
         def _prepare_dataset(split_name):
+            # Load dataset from URL
             data = pd.read_csv('https://raw.githubusercontent.com/PriyanJindal/spamdetection/main/inboxV4.csv')
 
             # Convert the 'spam' column to integer
@@ -51,7 +62,6 @@ class SpamDetectionModel(pl.LightningModule):
             else:
                 raise ValueError("Invalid split_name. Use 'train' or 'test'.")
 
-
             # Convert the pandas DataFrame to a datasets.Dataset object
             dataset = datasets.Dataset.from_pandas(df)
 
@@ -63,15 +73,19 @@ class SpamDetectionModel(pl.LightningModule):
 
             return dataset
 
+        # Split the dataset into training and testing sets
         self.train_ds, self.test_ds = map(_prepare_dataset, ('train', 'test'))
 
-
     def forward(self, input_ids):
-        mask = (input_ids != 0).float()  # Mask padding tokens
-        logits = self.model(input_ids, attention_mask=mask).logits  # Compute logits
+        # Mask padding tokens
+        mask = (input_ids != 0).float()
+        
+        # Compute logits
+        logits = self.model(input_ids, attention_mask=mask).logits
         return logits
 
     def training_step(self, batch, batch_idx):
+        # Extract input and labels from the batch
         input_ids = batch['input_ids']
         labels = batch['spam']
 
@@ -79,18 +93,21 @@ class SpamDetectionModel(pl.LightningModule):
         logits = self.forward(input_ids)
 
         # Calculate the loss
-        labels_tensor = th.tensor(labels, dtype=th.long) #converting labels into tensor
+        labels_tensor = th.tensor(labels, dtype=th.long)  # Convert labels into tensor
         loss = self.loss(logits, labels_tensor).mean()
         accuracy = (logits.argmax(-1) == labels_tensor).float().mean()
 
+        # Log the training loss and accuracy
         self.log('train_loss', loss)
         self.log('train_acc', accuracy)
 
         return {'loss': loss, 'train_acc': accuracy, 'log': {'train_loss': loss, 'train_acc': accuracy}}
 
-
     def validation_step(self, batch, batch_idx):
+        # Perform forward pass
         logits = self.forward(batch['input_ids'])
+        
+        # Calculate the loss and accuracy
         loss = self.loss(logits, batch['spam'])
         accuracy = (logits.argmax(-1) == batch['spam']).float()
         
@@ -98,49 +115,56 @@ class SpamDetectionModel(pl.LightningModule):
         filtered_indices = (logits.max(-1).values < 0.65)
         filtered_messages = [batch['messageBody'][i] for i in range(len(batch['messageBody'])) if filtered_indices[i]]
         
+        # Store loss and accuracy outputs for later averaging
         self.loss_outputs.extend(loss)
         self.acc_outputs.extend(accuracy)
         
         return {'loss': loss, 'acc': accuracy, 'filtered_messages': filtered_messages}
 
     def on_validation_epoch_end(self):
+        # Calculate average loss and accuracy
         loss_average = th.stack(self.loss_outputs).mean()
         acc_average = th.stack(self.acc_outputs).mean()
+        
+        # Create dictionary for logging
         out = {'val_loss': loss_average, 'val_acc': acc_average}
+        
+        # Clear loss and accuracy outputs for the next epoch
         self.loss_outputs.clear()
         self.acc_outputs.clear()
+        
         return {**out, 'log': out}
 
     def train_dataloader(self):
-        train_sampler = th.utils.data.RandomSampler(self.train_ds)  # Randomly samples data from the training dataset
+        # Randomly sample data from the training dataset
+        train_sampler = th.utils.data.RandomSampler(self.train_ds)
         train_loader = th.utils.data.DataLoader(self.train_ds, batch_size=self.batch_size, sampler=train_sampler)
         return train_loader
 
     def val_dataloader(self):
-        val_sampler = th.utils.data.SequentialSampler(self.test_ds)  # Sequentially samples data from the validation dataset
+        # Sequentially sample data from the validation dataset
+        val_sampler = th.utils.data.SequentialSampler(self.test_ds)
         val_loader = th.utils.data.DataLoader(self.test_ds, batch_size=self.batch_size, sampler=val_sampler)
         return val_loader
 
     def configure_optimizers(self):
+        # Configure Adam optimizer
         optimizer = th.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
     def predict_examples(self, examples):
+        # Load tokenizer
         tokenizer = AutoTokenizer.from_pretrained("mrm8488/bert-tiny-finetuned-sms-spam-detection")
 
+        # Iterate through examples for prediction
         for example in examples:
+            # Tokenize input text
             input_text = example['messageBody']
             input_ids = tokenizer.encode(input_text, max_length=self.seq_length, truncation=True, padding='max_length', return_tensors='pt')
+            
+            # Perform forward pass
             logits = self.forward(input_ids).squeeze()
 
+            # Get predicted label and probability
             predicted_label = th.argmax(logits).item()
-            predicted_prob = th.softmax(logits, dim=-1)[predicted_label].item()
-
-            if predicted_label == 0:
-                predicted_class = 'ham'
-            else:
-                predicted_class = 'spam'
-
-            print(f"Input Text: {input_text}")
-            print(f"Predicted Class: {predicted_class}")
-            print(f"Predicted Probability: {predicted_prob:.4f}\n")
+            predicted_prob
